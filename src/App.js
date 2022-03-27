20108ac53f325f8cb9e973199ba2faac49268f38
AlanNJ <alannpn75@gmail.com>
2022-03-27 14:29:47 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import Main from "./components/Main";
+import { ChakraProvider } from "@chakra-ui/react";
+import React, { useState, useEffect } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [number, setNumber] = useState();
+	const [loading, isLoading] = useState(false);
+
+	const getOtp = (e) => {
+		setNumber(e.target.value);
+	};
+	console.log(number);
+	return (
+		<div className="App">
+			<ChakraProvider>
+				<Main getOtp={getOtp} />
+			</ChakraProvider>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/Main.js
@@ -0,0 +1,60 @@
+import React from "react";
+import "./index.scss";
+import { useState, useEffect } from "react";
+import {
+	InputGroup,
+	InputLeftElement,
+	Input,
+	Button,
+	Spinner,
+} from "@chakra-ui/react";
+import { PhoneIcon, ArrowForwardIcon } from "@chakra-ui/icons";
+import axios from "axios";
+
+const Main = () => {
+	const [number, setNumber] = useState();
+	const [loading, isLoading] = useState(false);
+	const getOtp = (e) => {
+		const data = axios.post("http://localhost:5000/auth", { number });
+		console.log(data);
+	};
+	console.log(number);
+	return (
+		<>
+			<h1>
+				Demonstration of OTP registration using <b>Node</b> and <b>Express</b>
+			</h1>
+			<div className="container">
+				<InputGroup>
+					<InputLeftElement
+						pointerEvents="none"
+						children={<PhoneIcon color="gray.500" />}
+					/>
+					<Input
+						type="tel"
+						placeholder="Phone number"
+						size={"lg"}
+						width="60vw"
+						onChange={(e) => setNumber(e.target.value)}
+					/>
+				</InputGroup>
+				<Button
+					rightIcon={<ArrowForwardIcon />}
+					colorScheme="teal"
+					variant="outline"
+					onClick={getOtp}
+				>
+					Send OTP
+				</Button>
+				<Spinner
+					thickness="4px"
+					speed="0.65s"
+					emptyColor="gray.200"
+					color="blue.500"
+					size="xl"
+				/>
+			</div>
+		</>
+	);
+};
+export default Main;
